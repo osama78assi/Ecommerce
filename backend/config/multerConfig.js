b471c18ba28aeb9f3f601dfe688ff97ce760e730
Maher292 <83622306+Maher292@users.noreply.github.com>
2024-11-04 21:52:38 +0100

    Add files via upload
    multer config

---
+++ backend/config/multerConfig.js
@@ -0,0 +1,29 @@
+const multer = require('multer');
+const path = require("path");
+const fs = require("fs");
+const bcrypt = require("bcryptjs");
+
+
+const uploadDir = path.join(__dirname, "../uploads/slider-images/");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+// Configure multer to save uploaded images
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir); // Save in uploads/slider-images directory
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname); // Get the file extension
+    const uniqueName = `${Date.now()}_${Math.round(Math.random() * 1e9)}${ext}`; // Generate a unique name using timestamp and random number
+    cb(null, uniqueName);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // Limit the size to 5MB
+}).single("sliderImg");
+// Export the upload middleware function
+module.exports = upload; // Single file upload for "profilePic" field
